beefbabd163272a5ae6d033d60e9f5faebda3d94
Atalata <82236839+atalatable@users.noreply.github.com>
2025-05-06 19:20:18 +0200

    Add files via upload

---
+++ utils/appartement.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 3072x1920 --pos 3456x12 --rotate normal --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --scale 1.6x1.6
+feh --bg-fill ./Images/background-i3.jpg
